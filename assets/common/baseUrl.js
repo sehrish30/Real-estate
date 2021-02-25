03579de89aeaf3e8a82081de0ef098e29793df31
Sehrishah <sehrishwaheed98@gmail.com>
2021-02-26 00:47:55 +0300

    Change

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,6 +1,8 @@
 import { Platform } from "react-native";
-import { ANDROID_API, IOS_API } from "@env";
+// import { ANDROID_API, IOS_API } from "@env";
 
+const IOS_API = "http://localhost:3000/";
+const ANDROID_API = "http://37.131.117.29:3000/";
 let baseURL = "";
 console.log(IOS_API, ANDROID_API);
 {
--- screens/User/Forgot.js
+++ screens/User/Forgot.js
@@ -1,16 +1,18 @@
 import React, { useState, useLayoutEffect, useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Form from "../../Shared/Input/Form";
-// import SmoothPinCodeInput from "react-native-smooth-pincode-input";
-
+import SmoothPinCodeInput from "react-native-smooth-pincode-input";
+import { Text } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
 import {
   forgotUser,
   resetUserPassword,
 } from "../../Shared/Services/AuthServices";
 import { Button, Input } from "react-native-elements";
 import Toast from "react-native-toast-message";
+var { width, height } = Dimensions.get("window");
 
 const Forgot = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -42,6 +44,11 @@ const Forgot = ({ navigation }) => {
   };
 
   const validateEmail = async () => {
+    const res = await forgotUser({ email });
+    setPin(res.data.code);
+    setToken(res.data.token);
+    setShowForm(false);
+    setShowCode(true);
     if (
       email !== "" &&
       /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
@@ -68,17 +75,23 @@ const Forgot = ({ navigation }) => {
   };
 
   return (
-    <SafeAreaView>
-      <KeyboardAwareScrollView>
-        {showForm && (
+    <View>
+      <View style={styles.bg}>
+        <Image
+          style={styles.stretch}
+          source={require("../../assets/login.png")}
+          alt="bg"
+        />
+      </View>
+      <Text style={[styles.font, styles.heading]}>Stay connected with us</Text>
+      {showForm && (
+        <View>
           <Form
             email={email}
             setEmail={setEmail}
             loading={loading}
             name={"Change Password"}
           />
-        )}
-        {showForm && (
           <Button
             buttonStyle={styles.register}
             raised
@@ -86,68 +99,68 @@ const Forgot = ({ navigation }) => {
             loading={loading}
             onPress={validateEmail}
           />
-        )}
+        </View>
+      )}
 
-        {showPassword && (
-          <>
-            <Input
-              labelStyle={[styles.font, { color: "#a2d0c1" }]}
-              rightIcon={{
-                type: "font-awesome",
-                name: "key",
-                color: "#f8dc81",
-              }}
-              secureTextEntry
-              onChangeText={(value) => setPassword(value)}
-              label="Password"
-              value={password}
-            />
-            <Button
-              buttonStyle={styles.register}
-              raised
-              title={"Reset Password"}
-              loading={loading}
-              onPress={resetPassword()}
-            />
-          </>
-        )}
+      {showPassword && (
+        <>
+          <Input
+            labelStyle={[styles.font, { color: "#a2d0c1" }]}
+            rightIcon={{
+              type: "font-awesome",
+              name: "key",
+              color: "#f8dc81",
+            }}
+            secureTextEntry
+            onChangeText={(value) => setPassword(value)}
+            label="Password"
+            value={password}
+          />
+          <Button
+            buttonStyle={styles.register}
+            raised
+            title={"Reset Password"}
+            loading={loading}
+            onPress={resetPassword()}
+          />
+        </>
+      )}
 
-        {/* {showCode && (
-          <View style={styles.digit}>
-            <Text h4 style={styles.command}>
-              Enter your code
-            </Text>
-            <SmoothPinCodeInput
-              placeholder="⭑"
-              cellStyle={{
-                borderWidth: 2,
-                borderRadius: 24,
-                borderColor: "#a2d0c1",
-                backgroundColor: "#eff7e1",
-              }}
-              cellStyleFocused={{
-                borderColor: "#214151",
-                backgroundColor: "#eff7e1",
-              }}
-              textStyle={{
-                fontSize: 24,
-                color: "#a2d0c1",
-              }}
-              textStyleFocused={{
-                color: "214151",
-              }}
-              ref={pinInput}
-              value={code}
-              onTextChange={(code) => setCode(code)}
-              onFulfill={checkCode}
-              onBackspace={() => console.log("No more back.")}
-              cellSize={36}
-              codeLength={6}
-            />
-          </View>
-        )} */}
-      </KeyboardAwareScrollView>
-    </SafeAreaView>
+      {showCode && (
+        <View style={styles.digit}>
+          <Text h4 style={styles.command}>
+            Enter your code
+          </Text>
+          <SmoothPinCodeInput
+            placeholder="⭑"
+            cellStyle={{
+              borderWidth: 2,
+              borderRadius: 24,
+              borderColor: "#a2d0c1",
+              backgroundColor: "#eff7e1",
+            }}
+            cellStyleFocused={{
+              borderColor: "#214151",
+              backgroundColor: "#eff7e1",
+            }}
+            textStyle={{
+              fontSize: 24,
+              color: "#a2d0c1",
+            }}
+            textStyleFocused={{
+              color: "214151",
+            }}
+            ref={pinInput}
+            value={pin}
+            onTextChange={(code) => setPin(code)}
+            onFulfill={checkCode}
+            onBackspace={() => console.log("No more back.")}
+            cellSize={36}
+            codeLength={6}
+          />
+        </View>
+      )}
+    </View>
   );
 };
 
@@ -168,5 +181,21 @@ const styles = StyleSheet.create({
     marginTop: 5,
     backgroundColor: "#214151",
     color: "#214151",
+  },
+  stretch: {
+    width: 100,
+    height: 100,
+  },
+  bg: {
+    display: "flex",
+    margin: "auto",
+    alignItems: "center",
+  },
+  heading: {
+    textAlign: "center",
+    fontSize: 25,
+    color: "#214151",
+    marginBottom: 12,
+    marginTop: 5,
   },
 });
